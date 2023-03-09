c56a1e0656dc7a25ff9d253fbd26eb017d3bb864
theDeveloperShoon <seancotter04@gmail.com>
2023-03-09 16:37:07 -0500

    Stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <stdio.h>
+#include <SDL2/SDL.h>
 
 int main(){
-    std::cout << "Hello World of Games!\n";
+    SDL_Window *window = NULL;
+    SDL_Surface *screenSurface = NULL;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+	printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+    } else {
+	window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	if(window == NULL){
+	    printf("Window could not be created!  SDL Error: %s\n", SDL_GetError());
+	} else {
+	    screenSurface = SDL_GetWindowSurface(window);
+	    SDL_FillRect(screenSurface,NULL,SDL_MapRGB(screenSurface -> format, 0xFF, 0xFF, 0xFF));
+	    SDL_UpdateWindowSurface(window);
+	    SDL_Event e;
+	    bool quit = false;
+
+	    while(quit == false){
+		while(SDL_PollEvent(&e)){
+		    if(e.type == SDL_QUIT){
+			quit = true;
+		    }
+		}
+	    }
+	}
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,31 @@
+#OBJS specifies which files to compile as a part of the project
+OBJS = main.cpp
+
+#CC specifies which compiler
+CC = g++
+
+#SDL_CFLAGS includes all the libraries that SDL provides
+SDL_CFLAGS := $(shell sdl2-config --cflags)
+
+#SDL_LDFLAGS includes all the libraries that SDL provides
+SDL_LDFLAGS := $(shell sdl2-config --libs)
+
+#INCLUDE_PATHS specifies the additional include paths we'll need
+INCLUDE_PATHS = -Iinclude
+
+#LIBRARY_PATHS specifies the additional library paths we'll need
+LIBRARY_PATHS = -Llib
+
+#COMPILER_FLAGS specifies the additional compilation options we're using
+
+#LINKER_FLAGS specifies the libaries we're linking to
+LINKER_FLAGS = -lSDL2
+
+#OBJ_NAME specifies the name of our executable
+OBJ_NAME = GameExperiment
+
+#This is the target that compiles our executable
+all: $(OBJS)
+	$(CC) $(OBJS) $(SDL_CFLAGS) $(SDL_LDFLAGS) -o $(OBJ_NAME)
+clean:
+	rm -f $(OBJ_NAME) *.o
